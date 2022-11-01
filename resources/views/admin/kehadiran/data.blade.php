8548fb419fc355583ebf284d75c3f163f15a605f
Holymaiden <haikalwatampone3@gmail.com>
2022-11-01 10:26:41 +0800

    Update

--- resources/views/admin/kehadiran/data.blade.php
+++ resources/views/admin/kehadiran/data.blade.php
@@ -7,7 +7,7 @@
     <td>{{ $v->jam_masuk }}</td>
     <td>{{ $v->jam_keluar }}</td>
     <td>{{ $v->ket }}</td>
-    <th>{{ Helper::getDistance($v->lat,$v->long) }} / {{ Helper::getDistance($v->lat,$v->long) >= 50 ? 'Diluar Kampus' : 'Didalam Kampus' }}</th>
+    <th>{{ Helper::getDistance($v->lat,$v->long) >= 50 ? 'Diluar Kampus' : 'Didalam Kampus' }}</th>
     <td>{{ $v->surat_sakit ? $v->surat_sakit : 'Tidak Ada' }}</td>
     <th><a onclick="absenForm({{ $v->lat }}, {{ $v->long }})" class="btn btn-primary btn-icon icon-left text-white"><i class="fas fa-eye"></i></a></th>
     <td>
